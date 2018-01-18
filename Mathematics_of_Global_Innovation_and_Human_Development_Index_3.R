3f605620eab1f933a19d15ee245d1e6257d56ba7
Jeff Cromwell, PhD <designofharmony@gmail.com>
2018-01-17 17:17:21 -0700

    Add files via upload

---
+++ Mathematics_of_Global_Innovation_and_Human_Development_Index_3.R
@@ -0,0 +1,30 @@
+library(xtable)
+library(readxl)
+library(readr)
+
+#-------------------------------------Data---------------------------------------------------------------
+
+#-------------------------------------USDA Data Set-------------------------------------------------------
+ABBREV <- read_excel("sr28abxl/ABBREV.xlsx")
+View(ABBREV)
+Nutrition.df<-as.data.frame(ABBREV)
+Nutrition.Beans.df<-Nutrition.df[grep('BEANS,BLACK',Nutrition.df$Shrt_Desc),]
+#----------------------------------Transformations-------------------------------------------------------
+Nutrition.df<-Nutrition.df[with(Nutrition.df, 
+                                      order(Nutrition.df$Vit_A_IU,
+                                            decreasing=TRUE)),]
+Nutrition.Beans.Protein.df<-Nutrition.Beans.df[with(Nutrition.Beans.df, 
+                                                    order(Nutrition.Beans.df$`Protein_(g)`,
+                                                  decreasing=TRUE)),]
+Nutrition.Protein.df<-Nutrition.df[with(Nutrition.df, 
+                                              order(Nutrition.df$`Protein_(g)`,
+                                                    decreasing=TRUE)),]
+
+#----------------------------------Tables----------------------------------------------------------------
+Table.1<-xtable(Nutrition.Beans.df)
+
+#---------------------------------Figures---------------------------------------------------------------
+
+Figure.1<-hist(Nutrition.Protein.df$`Protein_(g)`)
+Figure.2<-plot(Nutrition.df$Vit_A_IU)
+#---------------------------------Function Library------------------------------------------------------
