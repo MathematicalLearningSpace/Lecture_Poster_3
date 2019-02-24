24eadab252da5e6fe9d51d6f484b1dbb51339c12
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-24 16:39:34 -0700

    Update Mathematics_of_Global_Innovation_and_Human_Development_Index_3.R

--- Mathematics_of_Global_Innovation_and_Human_Development_Index_3.R
+++ Mathematics_of_Global_Innovation_and_Human_Development_Index_3.R
@@ -1,7 +1,5 @@
-library(xtable)
-library(readxl)
-library(readr)
-
+#----------------------To Be Updated and Work in Progress for Classroom Examples---------------------------
+library(xtable);library(readxl);library(readr)
 #-------------------------------------Data---------------------------------------------------------------
 
 #-------------------------------------USDA Data Set-------------------------------------------------------
@@ -10,19 +8,11 @@ View(ABBREV)
 Nutrition.df<-as.data.frame(ABBREV)
 Nutrition.Beans.df<-Nutrition.df[grep('BEANS,BLACK',Nutrition.df$Shrt_Desc),]
 #----------------------------------Transformations-------------------------------------------------------
-Nutrition.df<-Nutrition.df[with(Nutrition.df, 
-                                      order(Nutrition.df$Vit_A_IU,
-                                            decreasing=TRUE)),]
-Nutrition.Beans.Protein.df<-Nutrition.Beans.df[with(Nutrition.Beans.df, 
-                                                    order(Nutrition.Beans.df$`Protein_(g)`,
-                                                  decreasing=TRUE)),]
-Nutrition.Protein.df<-Nutrition.df[with(Nutrition.df, 
-                                              order(Nutrition.df$`Protein_(g)`,
-                                                    decreasing=TRUE)),]
-
+Nutrition.df<-Nutrition.df[with(Nutrition.df, order(Nutrition.df$Vit_A_IU,decreasing=TRUE)),]
+Nutrition.Beans.Protein.df<-Nutrition.Beans.df[with(Nutrition.Beans.df, order(Nutrition.Beans.df$`Protein_(g)`,decreasing=TRUE)),]
+Nutrition.Protein.df<-Nutrition.df[with(Nutrition.df, order(Nutrition.df$`Protein_(g)`,decreasing=TRUE)),]
 #----------------------------------Tables----------------------------------------------------------------
 Table.1<-xtable(Nutrition.Beans.df)
-
 #---------------------------------Figures---------------------------------------------------------------
 
 Figure.1<-hist(Nutrition.Protein.df$`Protein_(g)`)
