e900e6a361a21f2fa5f8f602af1cf04d5a3c7e94
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-02-25 13:19:00 -0700

    Update Mathematical_Botany_1A.R

--- Mathematical_Botany_1A.R
+++ Mathematical_Botany_1A.R
@@ -29,6 +29,28 @@ plant.genera<-c("Apiaceae",
 BIEN_taxonomy_family(plant.genera[5])
 plant.brassica.trait<-BIEN_trait_family(plant.genera[5])
 
+plant.Apiaceae.trait<-BIEN_trait_family(plant.genera[1])
+plant.Araceae.trait<-BIEN_trait_family(plant.genera[2])
+plant.Arecaceae.trait<-BIEN_trait_family(plant.genera[3])
+plant.Asteraceae.trait<-BIEN_trait_family(plant.genera[4])
+plant.Brassicaceae.trait<-BIEN_trait_family(plant.genera[5])
+plant.Commelinaceae.trait<-BIEN_trait_family(plant.genera[6])
+plant.Ericaceae.trait<-BIEN_trait_family(plant.genera[7])
+plant.Liliaceae.trait<-BIEN_trait_family(plant.genera[8])
+plant.Amaryllidoideae.trait<-BIEN_trait_family(plant.genera[9])
+plant.Clusiaceae.trait<-BIEN_trait_family(plant.genera[10])
+plant.Euphorbiaceae.trait<-BIEN_trait_family(plant.genera[11])
+plant.Phyllanthaceae.trait<-BIEN_trait_family(plant.genera[12])
+plant.Picrodendraceae.trait<-BIEN_trait_family(plant.genera[13])
+plant.Apocynaceae.trait<-BIEN_trait_family(plant.genera[14])
+plant.Orchidaceae.trait<-BIEN_trait_family(plant.genera[15])
+plant.Poaceae.trait<-BIEN_trait_family(plant.genera[16])
+plant.Rosaceae.trait<-BIEN_trait_family(plant.genera[17])
+plant.Rubiaceae.trait<-BIEN_trait_family(plant.genera[18])
+plant.Rutaceae.trait<-BIEN_trait_family(plant.genera[19])
+plant.Sapindaceae.trait<-BIEN_trait_family(plant.genera[20])
+plant.tree.trait<-BIEN_trait_family(plant.genera[21])
+
 plant.brassica.trait.df<-as.data.frame(plant.brassica.trait)
 
 
@@ -46,8 +68,17 @@ Chile.df<-as.data.frame(BIEN_list_country("Chile"))
 Peru.df<-as.data.frame(BIEN_list_country("Peru"))
 Venezuela.df<-as.data.frame(BIEN_list_country("Venezuela"))
 
+#--------------------------------Formatted Students Notes for the Classroom--------------------
+Brassicaceace_Genera_Copy_Plant_Immune_System <- read_csv("Brassicaceace Genera Copy Plant Immune System.txt", 
+                                                          col_names = FALSE)
+View(Brassicaceace_Genera_Copy_Plant_Immune_System)
+
+Brassica.df<-data.frame(Brassicaceace_Genera_Copy_Plant_Immune_System)
+
+#--------------------------------Tables-----------------------------------
 
 
+#--------------------------------Figures-----------------------------------
 
 #-------------Function Template Library for Classroom Presentation and Modification---------------------
 f.1<-function(X)
