778d771ca0e80f7e707f2ee0c693a8cb4ed93c39
Jeff Cromwell, PhD <designofharmony@gmail.com>
2019-03-09 06:33:30 -0700

    Update Food_nutrition_1.R

--- Food_nutrition_1.R
+++ Food_nutrition_1.R
@@ -1,1 +1,11 @@
 #Title: Food Nutrition Examples for the Classroom
+
+library(readr)
+library(dplyr)
+setwd("BFPD_csv_07132018")
+#---------------------------------Data-----------------------------------------------------------------------------------------------#
+#---https://data.nal.usda.gov/dataset/usda-branded-food-products-database/resource/c929dc84-1516-4ac7-bbb8-c0c191ca8cec--------------#
+Nutrients.df <- as.data.frame(read_csv("Nutrients.csv"))
+Products.df <- as.data.frame(read_csv("Products.csv"))
+Derivation_Code_Description.df <- as.data.frame(read_csv("Derivation_Code_Description.csv"))
+Serving_size.df <- as.data.frame(read_csv("Serving_size.csv"))
